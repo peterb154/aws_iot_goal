159fa19a126e30f31bb7d3500380b3eeddc90756
Brian Peterson <brian@petersons.com>
2018-12-05 15:45:58 -0600

    updated docs

--- loop.sh
+++ loop.sh
@@ -5,12 +5,14 @@ export AWS_DEFAULT_REGION=us-east-1
 
 set -e
 
+# Install the python requirements
 if [ ! -d venv ]; then
     virtualenv venv
     . ./venv/bin/activate
     pip install -r requirements.txt
 fi
 
+# Create a place to do the work (~/tmp/greengo)
 mkdir -p ~/tmp
 cd ~/tmp
 if [ ! -d greengo ]; then
@@ -18,20 +20,31 @@ if [ ! -d greengo ]; then
 fi
 cd greengo
 
+# Get the counter if it exists
 if [ -f ~/tmp/count.txt ]; then
     count=`cat ~/tmp/count.txt`
 else
     count=1
 fi
-while [ $count -lt 60 ]; do
+
+# Main loop
+while [ $count -lt 70 ]; do
     echo "----------------COUNT $count"
+    # Put a unique group and core name in the config file
     sed -i -e s/"^  name:.*"/"  name: GreengoGroup_${count}"/ greengo.yaml
     sed -i -e s/"^  - name: Greengo.*"/"  - name: GreengoGroup_${count}_core_${count}"/ greengo.yaml
-    count=$(($count+1));
+    # Create a greengrass group & core in AWS
     greengo create
+    # Bring up the local VM core emulator
     vagrant up
+    # Deploy the lamda to the core
     greengo deploy
+    # Remove the greengrass group & core from AWS
     greengo remove
+    # Kill the local core emulator
     vagrant destroy -f
+    # Increment the counter
+    count=$(($count+1));
+    # Update the counter file (in case the process dies)
     echo $count > ~/tmp/count.txt
 done
